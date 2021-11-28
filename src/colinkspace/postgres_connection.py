829c95ddc0ad04bcc77e059d06872ac56acd0618
David Kuda <david.kuda@pm.me>
2021-11-28 21:56:28 +0100

    feat: Create a class that handles db connection

---
+++ src/colinkspace/postgres_connection.py
@@ -0,0 +1,31 @@
+import psycopg2
+
+from src.utils.main import parse_config_file
+
+
+class PostgresConnection:
+    def __init__(self, postgres_config = None):
+        if postgres_config is None:
+            postgres_config = parse_config_file()["POSTGRES"]
+        self._postgres_config = postgres_config
+        self.conn = self.connect_to_db()
+    
+    def connect_to_db(self):
+        postgres_config = self._postgres_config
+        conn = psycopg2.connect(
+            host=postgres_config["HOST"],
+            port=postgres_config["PORT"],
+            user=postgres_config["USER"],
+            password=postgres_config["PASSWORD"],
+            database=postgres_config["DATABASE"]
+        )
+        return conn
+
+    def execute_sql(self, query: str):
+        cur = self.conn.cursor()
+        cur.execute(query)
+        self.conn.commit()
+        cur.close()
+
+    def process_frontend_input():
+        pass
