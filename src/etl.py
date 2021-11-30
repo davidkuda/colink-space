f54bde0b72c238a06f968c9931dafb76853a040d
David Kuda <david.kuda@pm.me>
2021-11-30 22:50:02 +0100

    Make a new plan for the ETL

--- src/etl.py
+++ src/etl.py
@@ -1,15 +1,17 @@
-from src.generate_mock_data import (generate_users, create_hacker_news_links_csv_file,
-                                    write_app_data_files)
-
+from src.utils.generate_mock_data import (
+    generate_random_users, create_hacker_news_links_csv_file, write_app_data_files
+)
 
 def main():
-    generate_users(100)
     create_hacker_news_links_csv_file()
-    write_app_data_files(100)
-    
+    generate_random_users(100)
     # delete tables
     # create tables
     # insert into tables
+    # -- insert new user
+    # -- create a new default space for new user
+    # get a list of space_ids 
+    write_app_data_files(100)
     # join and see which user has most shares
     # join and see which links was shared most
 
