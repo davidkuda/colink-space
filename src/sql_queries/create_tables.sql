d578c9b647428dead61cf8bea0199dcf5405830b
David Kuda <david.kuda@pm.me>
2021-12-05 12:37:22 +0100

    refactor: Use "UUID" instead of "serial"

--- src/sql_queries/create_tables.sql
+++ src/sql_queries/create_tables.sql
@@ -1,58 +1,58 @@
 CREATE TABLE IF NOT EXISTS users {
-    user_id serial NOT NULL,
+    user_id UUID NOT NULL,
     name varchar(256),
     email varchar(256),
 };
 
 CREATE TABLE IF NOT EXISTS countries {
-    country_id serial NOT NULL PRIMARY KEY,
+    country_id UUID NOT NULL PRIMARY KEY,
     country varchar(256) NOT NULL
 };
 
 CREATE TABLE IF NOT EXISTS users_countries_map {
-    country_id integer NOT NULL REFERENCES(countries.country_id),
-    user_id integer NOT NULL REFERENCES(users.user_id)
+    country_id UUID NOT NULL REFERENCES(countries.country_id),
+    user_id UUID NOT NULL REFERENCES(users.user_id)
 };
 
 CREATE TABLE IF NOT EXISTS professions {
-    profession_id serial PRIMARY KEY,
+    profession_id UUID PRIMARY KEY,
     profession varchar(256)
 };
 
 CREATE TABLE IF NOT EXISTS users_professions_map {
-    user_id integer NOT NULL REFERENCES(users.user_id),
-    profession_id integer NOT NULL REFERENCES(professions.profession_id)
+    user_id UUID NOT NULL REFERENCES(users.user_id),
+    profession_id UUID NOT NULL REFERENCES(professions.profession_id)
 };
 
 CREATE TABLE IF NOT EXISTS interests {
-    interest_id serial NOT NULL PRIMARY KEY,
+    interest_id UUID NOT NULL PRIMARY KEY,
     interest varchar(256) NOT NULL
 };
 
 CREATE TABLE IF NOT EXISTS users_interests_map {
-    user_id integer NOT NULL REFERENCES(users.user_id),
-    interest_id integer NOT NULL REFERENCES(interests.interest_id)
+    user_id UUID NOT NULL REFERENCES(users.user_id),
+    interest_id UUID NOT NULL REFERENCES(interests.interest_id)
 };
 
 
 CREATE TABLE IF NOT EXISTS spaces {
-    space_id serial NOT NULL PRIMARY KEY,
+    space_id UUID NOT NULL PRIMARY KEY,
     space_name varchar(256),
     creation_date date NOT NULL
 };
 
 CREATE TABLE IF NOT EXISTS space_owners {
-    space_id serial NOT NULL PRIMARY KEY,
-    user_id integer REFERENCES(users.user_id)
+    space_id UUID NOT NULL PRIMARY KEY,
+    user_id UUID REFERENCES(users.user_id)
 };
 
 CREATE TABLE IF NOT EXISTS space_contributors {
-    space_id integer REFERENCES(spaces.space_id),
-    user_id integer REFERENCES(users.user_id)
+    space_id UUID REFERENCES(spaces.space_id),
+    user_id UUID REFERENCES(users.user_id)
 };
 
 CREATE TABLE IF NOT EXISTS links {
-    url_id serial NOT NULL PRIMARY KEY,
+    url_id UUID NOT NULL PRIMARY KEY,
     url varchar(512) NOT NULL,
     meta_title varchar(256),
     meta_description varchar(256),
@@ -60,10 +60,10 @@ CREATE TABLE IF NOT EXISTS links {
 };
 
 CREATE TABLE IF NOT EXISTS posts {
-    post_id serial NOT NULL PRIMARY KEY,
-    url_id integer REFERENCES(links.url),
-    space_id integer NOT NULL REFERENCES(spaces.space_id),
-    user_id integer NOT NULL REFERENCES(users.user_id),
+    post_id UUID NOT NULL PRIMARY KEY,
+    url_id UUID REFERENCES(links.url),
+    space_id UUID NOT NULL REFERENCES(spaces.space_id),
+    user_id UUID NOT NULL REFERENCES(users.user_id),
     description varchar(512),
     date date
 };
