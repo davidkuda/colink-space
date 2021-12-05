daaa4d860be267f239c6d7b57bb4140679f290b8
David Kuda <david.kuda@pm.me>
2021-12-05 22:38:46 +0100

    fix: Use hyphens, not curly braces

--- src/sql_queries/create_tables.sql
+++ src/sql_queries/create_tables.sql
@@ -1,38 +1,38 @@
-CREATE TABLE IF NOT EXISTS users {
+CREATE TABLE IF NOT EXISTS users (
     user_id UUID NOT NULL,
     name varchar(256),
     email varchar(256)
-};
+);
 
-CREATE TABLE IF NOT EXISTS spaces {
+CREATE TABLE IF NOT EXISTS spaces (
     space_id UUID NOT NULL PRIMARY KEY,
     space_name varchar(256),
     creation_date date NOT NULL
-};
+);
 
-CREATE TABLE IF NOT EXISTS space_owners {
+CREATE TABLE IF NOT EXISTS space_owners (
     space_id UUID NOT NULL PRIMARY KEY,
     user_id UUID REFERENCES(users.user_id)
-};
+);
 
-CREATE TABLE IF NOT EXISTS space_contributors {
+CREATE TABLE IF NOT EXISTS space_contributors (
     space_id UUID REFERENCES(spaces.space_id),
     user_id UUID REFERENCES(users.user_id)
-};
+);
 
-CREATE TABLE IF NOT EXISTS links {
+CREATE TABLE IF NOT EXISTS links (
     url_id UUID NOT NULL PRIMARY KEY,
     url varchar(512) NOT NULL,
     meta_title varchar(256),
     meta_description varchar(256),
     image_url varchar(256)
-};
+);
 
-CREATE TABLE IF NOT EXISTS posts {
+CREATE TABLE IF NOT EXISTS posts (
     post_id UUID NOT NULL PRIMARY KEY,
     url_id UUID REFERENCES(links.url),
     space_id UUID NOT NULL REFERENCES(spaces.space_id),
     user_id UUID NOT NULL REFERENCES(users.user_id),
     description varchar(512),
     date date
-};
+);
