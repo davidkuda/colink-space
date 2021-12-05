f71e189cd02f3605963dd103add4d5ee83e9e7a6
David Kuda <david.kuda@pm.me>
2021-12-05 22:42:23 +0100

    fix: Use correct syntax with "REFERENCES"

--- src/sql_queries/create_tables.sql
+++ src/sql_queries/create_tables.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS users (
-    user_id UUID NOT NULL,
+    user_id UUID PRIMARY KEY NOT NULL,
     name varchar(256),
     email varchar(256)
 );
@@ -12,12 +12,12 @@ CREATE TABLE IF NOT EXISTS spaces (
 
 CREATE TABLE IF NOT EXISTS space_owners (
     space_id UUID NOT NULL PRIMARY KEY,
-    user_id UUID REFERENCES(users.user_id)
+    user_id UUID REFERENCES users(user_id)
 );
 
 CREATE TABLE IF NOT EXISTS space_contributors (
-    space_id UUID REFERENCES(spaces.space_id),
-    user_id UUID REFERENCES(users.user_id)
+    space_id UUID REFERENCES spaces(space_id),
+    user_id UUID REFERENCES users(user_id)
 );
 
 CREATE TABLE IF NOT EXISTS links (
@@ -30,9 +30,9 @@ CREATE TABLE IF NOT EXISTS links (
 
 CREATE TABLE IF NOT EXISTS posts (
     post_id UUID NOT NULL PRIMARY KEY,
-    url_id UUID REFERENCES(links.url),
-    space_id UUID NOT NULL REFERENCES(spaces.space_id),
-    user_id UUID NOT NULL REFERENCES(users.user_id),
+    url_id UUID REFERENCES links(url_id),
+    space_id UUID NOT NULL REFERENCES spaces(space_id),
+    user_id UUID NOT NULL REFERENCES users(user_id),
     description varchar(512),
     date date
 );
